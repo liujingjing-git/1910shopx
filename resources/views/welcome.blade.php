2cb999e3821cc8a93e272af17e1b9375a6d21b94
root <root@localhost.localdomain>
2020-07-16 07:40:32 +0000

    Update++

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
- 			{{env['APP_ENV']}}
+ 			{{env('APP_ENV')}}
                 </div>
 
                 <div class="links">
